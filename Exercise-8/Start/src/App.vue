a560b65210e7f25d38f13413fb3b608943091674
Max Donchenko <Maksym.Donchenko@cloudmade.com>
2019-10-21 16:52:18 +0300

    ex 8

---
+++ Exercise-8/Start/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <br>
+        <button @click="c = 'Blue'" class="btn btn-primary">Load Blue Template</button>
+        <button @click="c = 'Green'" class="btn btn-success">Load Green Template</button>
+        <button @click="c = 'Red'" class="btn btn-danger">Load Red Template</button>
+        <hr>
+        <template v-if="c === 'Blue'">
+          <component is="app-blue"></component>
+        </template>
+        <template v-else-if="c === 'Green'">
+          <component is="app-green"></component>
+        </template>
+        <template v-else>
+          <component is="app-red"></component>
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Blue from './components/Blue.vue';
+  import Green from './components/Green.vue';
+  import Red from './components/Red.vue';
+
+  export default {
+    components: {
+      appBlue: Blue,
+      appGreen: Green,
+      appRed: Red,
+    },
+    data() {
+      return {
+        c: 'Blue',
+      };
+    },
+  };
+</script>
+
+<style>
+</style>

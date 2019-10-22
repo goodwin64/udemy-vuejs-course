263cf4bc02e5ba9456591bc2f00da02b26cf12f8
Max Donchenko <Maksym.Donchenko@cloudmade.com>
2019-10-22 12:04:47 +0300

    ex 9

--- Exercise-9/Start/src/App.vue
+++ Exercise-9/Start/src/App.vue
@@ -1,8 +1,83 @@
 <template>
     <div class="container">
-        <form>
+        <form v-show="!formSubmitted">
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <label for="name" class="col-xs-12">
+                        <span class="col-xs-4">Name:</span>
+                        <div class="col-xs-8">
+                            <input type="text" id="name" v-model="name" class="form-control" />
+                        </div>
+                    </label>
+
+                    <label for="surname" class="col-xs-12">
+                        <span class="col-xs-4">Surname:</span>
+                        <div class="col-xs-8">
+                            <input type="text" id="surname" v-model="surname" class="form-control" />
+                        </div>
+                    </label>
+
+                    <label for="fullname" class="col-xs-12">
+                        <span class="col-xs-4" title="Suggestion based on name and surname">Fullname (?):</span>
+                        <div class="col-xs-8">
+                            <control-full-name id="fullname" v-model="computedFullname"></control-full-name>
+                        </div>
+                    </label>
+
+                    <label for="email" class="col-xs-12">
+                        <span class="col-xs-4" title="Suggestion based on name and surname">Email (?):</span>
+                        <div class="col-xs-8">
+                            <input
+                                    type="text"
+                                    id="email"
+                                    :value="email"
+                                    class="form-control"
+                                    @focusin="onEmailFocus"
+                                    @change="onEmailChange"
+                                    autocomplete="email"
+                            />
+                        </div>
+                    </label>
+
+                    <label for="password" :class="['col-xs-12', { 'has-error': error.password }]">
+                        <span class="col-xs-4">Password:</span>
+                        <div class="col-xs-8">
+                            <input
+                                    type="password"
+                                    id="password"
+                                    v-model="password"
+                                    class="form-control"
+                                    @focusout="touched.password = true"
+                                    autocomplete="new-password"
+                            />
+                        </div>
+                        <span v-show="error.password"
+                              class="col-xs-12 text-danger">Should be 8+ characters length</span>
+                    </label>
+
+                    <label for="repeatPassword" class="col-xs-12" :class="{ 'has-error': error.repeatPassword }">
+                        <span class="col-xs-4">Repeat password:</span>
+                        <div class="col-xs-8">
+                            <input
+                                    type="password"
+                                    id="repeatPassword"
+                                    v-model="repeatPassword"
+                                    class="form-control"
+                                    autocomplete="new-password"
+                            />
+                        </div>
+                        <span v-show="error.repeatPassword" class="col-xs-12 text-danger">Should match password</span>
+                    </label>
+
+                    <label for="storeData" class="col-xs-12">
+                        <span class="col-xs-4">Store data?</span>
+                        <div class="col-xs-8">
+                            <input type="checkbox" id="storeData" v-model="storeData" class="form-control" />
+                        </div>
+                    </label>
+
+                    <button v-show="!formSubmitted" type="submit" @click.prevent.stop="formSubmitted = true">Submit
+                    </button>
                     <!-- Exercise 1 -->
                     <!-- Create a Signup Form where you retrieve the following Information -->
                     <!-- Full Name (First Name + Last Name) -->
@@ -20,18 +95,19 @@
                 </div>
             </div>
         </form>
+        <button v-show="formSubmitted" type="button" @click="formSubmitted = false">Back to form</button>
         <hr>
-        <div class="row">
+        <div class="row" v-show="formSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{fullname}}</p>
+                        <p>Mail: {{email}}</p>
+                        <p>Password: {{password}}</p>
+                        <p>Store in Database?: {{storeData}}</p>
                     </div>
                 </div>
             </div>
@@ -40,8 +116,75 @@
 </template>
 
 <script>
-    export default {
-    }
+  import ControlFullName from './ControlFullName.vue';
+
+  export default {
+    data() {
+      return {
+        formSubmitted: false,
+        name: '',
+        surname: '',
+        fullname: '',
+        hostname: '',
+        password: '',
+        repeatPassword: '',
+        touched: {
+          password: false,
+        },
+        storeData: false,
+      };
+    },
+    components: {
+      'control-full-name': ControlFullName,
+    },
+    computed: {
+      computedFullname: {
+        get() {
+          return this.fullname || (this.name + ' ' + this.surname);
+        },
+        set(fullname) {
+          this.fullname = fullname;
+        },
+      },
+      email() {
+        return `${this.name}.${this.surname}@${this.hostname}`.toLowerCase();
+      },
+      errorPassword() {
+        return this.password.length < 8;
+      },
+      errorRepeatPassword() {
+        return this.repeatPassword !== this.password;
+      },
+      error() {
+        return {
+          password: this.touched.password && this.errorPassword,
+          repeatPassword: this.touched.password && this.errorRepeatPassword,
+        };
+      },
+    },
+    methods: {
+      onEmailFocus(event) {
+        const emailBeforeHostLength = this.email.indexOf('@') + 1;
+        event.target.setSelectionRange(emailBeforeHostLength, 1000);
+      },
+      onEmailChange(event) {
+        const email = event.target.value;
+        const hostname = email.slice(email.indexOf('@') + 1);
+        this.hostname = hostname;
+      },
+      updateFullName(name, surname) {
+        this.fullname = name + ' ' + surname;
+      },
+    },
+    watch: {
+      name(newName, oldName) {
+        this.updateFullName(newName, this.surname);
+      },
+      surname(newSurname, oldSurname) {
+        this.updateFullName(this.name, newSurname);
+      },
+    },
+  };
 </script>
 
 <style>
---
+++ Exercise-9/Start/src/ControlFullName.vue
@@ -0,0 +1,13 @@
+<template>
+  <input type="text" class="form-control" @change="handleChange" />
+</template>
+
+<script>
+  export default {
+    methods: {
+      handleChange(event) {
+        this.$emit('input', event.target.value);
+      }
+    }
+  }
+</script>

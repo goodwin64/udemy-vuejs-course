6f3c7adccefb681068773cbd87a2d2443de8c822
Max Donchenko <Maksym.Donchenko@cloudmade.com>
2019-10-26 22:19:13 +0300

    ex 18: init

---
+++ Exercise-18/src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header class="header">
+    <div class="header-left">
+      <h2 class="logo">Stock trader</h2>
+      <router-link to="/">Home</router-link>
+      <router-link to="/portfolio">Portfolio</router-link>
+      <router-link to="/stocks">Stocks</router-link>
+    </div>
+
+    <div class="header-right">
+      <button>End day</button>
+      <button>Save & load</button>
+      <p>Funds: $10'000</p>
+    </div>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+  };
+</script>
+
+<style scoped>
+  .header {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .header-left,
+  .header-right {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
